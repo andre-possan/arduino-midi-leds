a3c6c73287b2b340a4f54993ae83ed9101940a2c
andre <andrecorsopossan@gmail.com>
2021-12-13 20:24:46 -0300

    Removido Ifs, substituidos por um for

--- test-midi.ino
+++ test-midi.ino
@@ -1,200 +1,84 @@
-#include <MIDI.h>
+/* Codigo escrito por André, sou iniciante em C++, caso encontre um erro ou tenha uma recomendação, não hesite em mandar uma mensagem! https://github.com/andre-possan :D
+  Atenção: Para este código funcionar você deve utilizar o Hairless MIDI https://projectgus.github.io/hairless-midiserial
+  Utilizando o Hairless você deve alterar o arquivo serialMIDI.h dentro da pasta da biblioteca e definir a propriedade BaudRate para 115200:
+  static const long BaudRate = 115200;
+*/
 
-#define LED 13
-#define LED2 12
-#define LED3 11
-#define LED4 10
-#define LED5 9
-#define LED6 8
-#define LED7 7
-#define LED8 6
-#define LED9 5
-#define LED10 4
-#define LED11 3
+#include <MIDI.h> // Incluindo a biblioteca que utilizaremos para receber informações MIDI.
 
-MIDI_CREATE_DEFAULT_INSTANCE();
+int Leds[] = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3}; // Definindo as portas que temos leds conectados
+int quatidadeDeLeds = sizeof(Leds);
+MIDI_CREATE_DEFAULT_INSTANCE(); // Parâmetro da biblioteca que inicia uma instância MIDI
 
 void setup()
 {
-  pinMode(LED, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
-  pinMode(LED5, OUTPUT);
-  pinMode(LED6, OUTPUT);
-  pinMode(LED7, OUTPUT);
-  pinMode(LED8, OUTPUT);
-  pinMode(LED9, OUTPUT);
-  pinMode(LED10, OUTPUT);
-  pinMode(LED11, OUTPUT);
-  MIDI.begin(4); // MIDI_CHANNEL_OMNI
+  for (int contador = 0; contador < quatidadeDeLeds; contador++)
+  { // Definindo as portas dos leds como saída
+    pinMode(Leds[contador], OUTPUT);
+  }
+
+  MIDI.begin(4); // "Escutando" apenas o canal 4, para todos os canais utilize: MIDI_CHANNEL_OMNI
   Serial.begin(115200);
-  MIDI.setHandleNoteOn(MyHandleNoteOn);
-  MIDI.setHandleNoteOff(MyHandleNoteOff);
+  MIDI.setHandleNoteOn(MyHandleNoteOn);  // Função que será executada quando uma tecla for pressionada
+  MIDI.setHandleNoteOff(MyHandleNoteOn); // Função que será executada quando uma tecla parar de ser pressionada.
 }
 
 void loop()
 {
-  MIDI.read();
+  MIDI.read(); // O Arduino sempre ficara lendo informações MIDI
 }
 
-int ligado;
-int ligado2;
-int ligado3;
-int ligado4;
-int ligado5;
-int ligado6;
-int ligado7;
-int ligado8;
-int ligado9;
-int ligado10;
-int ligado11;
-
-int verifica(int onLed, int ligado)
+// Função que só desiliga o led se todas as teclas referentes a ele não forem mais pressionadas:
+int verifica(int Led, int ligado)
 {
   if (ligado > 0)
   {
-    digitalWrite(onLed, HIGH);
+    digitalWrite(Led, HIGH);
   }
   else if (ligado <= 0)
   {
-    digitalWrite(onLed, LOW);
+    digitalWrite(Led, LOW);
   }
 }
+int lig1;
+int lig2;
+int lig3;
+int lig4;
+int lig5;
+int lig6;
+int lig7;
+int lig8;
+int lig9;
+int lig10;
+int lig11;
 
+int ligado[] = {lig1, lig2, lig3, lig4, lig5, lig6, lig7, lig8, lig9, lig10, lig11};
+
+/*Função que recebera os dados MIDI:
+ * channel = canal MIDI,
+ * pitch = número identificador da tecla,
+ * velocity = a "força" com que a tecla foi pressionada varia de 0 a 127*/
 void MyHandleNoteOn(byte channel, byte pitch, byte velocity)
 {
-  if (pitch >= 36 && pitch <= 41 && velocity > 0)
+  for (int contador = 0; contador < quatidadeDeLeds; contador++)
   {
-    ligado = ligado + 1;
-  }
-  verifica(LED, ligado);
 
-  if (pitch >= 42 && pitch <= 47 && velocity > 0)
-  {
-    ligado2 = ligado2 + 1;
-  }
-  verifica(LED2, ligado2);
+    // Estabelecendo limites para cada led, Ex: teclas 36 a 40, vão ligar o Led1
+    int minimo = (contador * 6) + 36;
+    int maximo = minimo + 5;
 
-  if (pitch >= 48 && pitch <= 53 && velocity > 0)
-  {
-    ligado3 = ligado3 + 1;
+    // Verificando se a tecla esta dentro dos limites
+    if (pitch >= minimo && pitch <= maximo)
+    {
+      if (velocity > 0)
+      {
+        ligado[contador] = ligado[contador] + 1;
+      }
+      else
+      {
+        ligado[contador] = ligado[contador] - 1;
+      }
+      verifica(Leds[contador], ligado[contador]); // Chamando a função para verificar e ligar o Led
+    }
   }
-  verifica(LED3, ligado3);
-
-  if (pitch >= 54 && pitch <= 59 && velocity > 0)
-  {
-    ligado4 = ligado4 + 1;
-  }
-  verifica(LED4, ligado4);
-
-  if (pitch >= 60 && pitch <= 65 && velocity > 0)
-  {
-    ligado5 = ligado5 + 1;
-  }
-  verifica(LED5, ligado5);
-
-  if (pitch >= 66 && pitch <= 71 && velocity > 0)
-  {
-    ligado6 = ligado6 + 1;
-  }
-  verifica(LED6, ligado6);
-
-  if (pitch >= 72 && pitch <= 77 && velocity > 0)
-  {
-    ligado7 = ligado7 + 1;
-  }
-  verifica(LED7, ligado7);
-
-  if (pitch >= 78 && pitch <= 83 && velocity > 0)
-  {
-    ligado8 = ligado8 + 1;
-  }
-  verifica(LED8, ligado8);
-
-  if (pitch >= 84 && pitch <= 89 && velocity > 0)
-  {
-    ligado9 = ligado9 + 1;
-  }
-  verifica(LED9, ligado9);
-
-  if (pitch >= 90 && pitch <= 95 && velocity > 0)
-  {
-    ligado10 = ligado10 + 1;
-  }
-  verifica(LED10, ligado10);
-
-  if (pitch >= 96 && pitch <= 97 && velocity > 0)
-  {
-    ligado11 = ligado11 + 1;
-  }
-  verifica(LED11, ligado11);
-}
-void MyHandleNoteOff(byte channel, byte pitch, byte velocity)
-{
-  if (pitch >= 36 && pitch <= 41 && velocity == 0)
-  {
-    ligado = ligado - 1;
-  }
-  verifica(LED, ligado);
-
-  if (pitch >= 42 && pitch <= 47 && velocity == 0)
-  {
-    ligado2 = ligado2 - 1;
-  }
-  verifica(LED2, ligado2);
-
-  if (pitch >= 48 && pitch <= 53 && velocity == 0)
-  {
-    ligado3 = ligado3 - 1;
-  }
-  verifica(LED3, ligado3);
-
-  if (pitch >= 54 && pitch <= 59 && velocity == 0)
-  {
-    ligado4 = ligado4 - 1;
-  }
-  verifica(LED4, ligado4);
-
-  if (pitch >= 60 && pitch <= 65 && velocity == 0)
-  {
-    ligado5 = ligado5 - 1;
-  }
-  verifica(LED5, ligado5);
-
-  if (pitch >= 66 && pitch <= 71 && velocity == 0)
-  {
-    ligado6 = ligado6 - 1;
-  }
-  verifica(LED6, ligado6);
-
-  if (pitch >= 72 && pitch <= 77 && velocity == 0)
-  {
-    ligado7 = ligado7 - 1;
-  }
-  verifica(LED7, ligado7);
-
-  if (pitch >= 78 && pitch <= 83 && velocity == 0)
-  {
-    ligado8 = ligado8 - 1;
-  }
-  verifica(LED8, ligado8);
-
-  if (pitch >= 84 && pitch <= 89 && velocity == 0)
-  {
-    ligado9 = ligado9 - 1;
-  }
-  verifica(LED9, ligado9);
-
-  if (pitch >= 90 && pitch <= 95 && velocity == 0)
-  {
-    ligado10 = ligado10 - 1;
-  }
-  verifica(LED10, ligado10);
-
-  if (pitch >= 96 && pitch <= 97 && velocity == 0)
-  {
-    ligado11 = ligado11 - 1;
-  }
-  verifica(LED11, ligado11);
 }
